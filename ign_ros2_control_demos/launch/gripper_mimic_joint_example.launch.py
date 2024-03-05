5b778e7ba305a87bf3a5ae02eed9ad52e224eef9
Sebastian Castro <sebas.a.castro@gmail.com>
2024-03-05 01:53:04 -0500

    Change verbosity level

--- ign_ros2_control_demos/launch/gripper_mimic_joint_example.launch.py
+++ ign_ros2_control_demos/launch/gripper_mimic_joint_example.launch.py
@@ -88,7 +88,7 @@ def generate_launch_description():
             PythonLaunchDescriptionSource(
                 [os.path.join(get_package_share_directory('ros_gz_sim'),
                               'launch', 'gz_sim.launch.py')]),
-            launch_arguments=[('ign_args', [' -r -v 4 empty.sdf'])]),
+            launch_arguments=[('ign_args', [' -r -v 3 empty.sdf'])]),
         RegisterEventHandler(
             event_handler=OnProcessExit(
                 target_action=ignition_spawn_entity,
